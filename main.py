c5bf8480d2462e53b7af1aa2c87f5aec0db028a2
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2023-08-02 12:42:29 +0900

    basic example

--- main.py
+++ main.py
@@ -5,9 +5,24 @@ Here's our first attempt at using data to create a table:
 
 import streamlit as st
 import pandas as pd
+import seaborn as sns
+
 df = pd.DataFrame({
-    'first column': [1, 2, 3, 4],
-    'second column': [10, 20, 30, 40]
+    "x": [1, 2, 3, 4],
+    "x2": [11, 12, 13, 14],
+    "y": [10, 20, 30, 40]
 })
 
-df
+
+option = st.selectbox("which column ?", ["x", "x2"])
+
+plot = sns.lineplot(x=option, y="y", data=df)
+
+
+st.pyplot(plot.get_figure())
+
+"st.write"
+st.write(df)
+
+"st.table"
+st.table(df)
