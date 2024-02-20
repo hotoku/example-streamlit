fb59821b172a1663c301195cda6cbc728f672a67
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2024-02-20 23:39:24 +0900

    [upd] this works

--- main.py
+++ main.py
@@ -4,22 +4,18 @@ Here's our first attempt at using data to create a table:
 """
 
 import streamlit as st
-import pandas as pd
 import seaborn as sns
 
-df = pd.DataFrame({
-    "x": [1, 2, 3, 4],
-    "x2": [11, 12, 13, 14],
-    "y": [10, 20, 30, 40]
-})
+from exst.mod import get_df
 
+df = get_df()
 
 option = st.selectbox("which column ?", ["x", "x2"])
 
 plot = sns.lineplot(x=option, y="y", data=df)
 
 
-st.pyplot(plot.get_figure())
+st.pyplot(plot.get_figure())  # type: ignore
 
 "st.write"
 st.write(df)
